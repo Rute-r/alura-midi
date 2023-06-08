dc2ab2d3fa137e33e8df1cbaec240349a3115b89
Rute-r <rute_ke@hotmail.com>
2023-06-08 14:54:17 -0300

    loop while

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  contador = contador + 1;
+}
