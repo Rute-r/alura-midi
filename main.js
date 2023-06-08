70b2601eab8c4d027879da91a65c3003ab619d7e
Rute-r <rute_ke@hotmail.com>
2023-06-08 15:07:49 -0300

    utilizando loop for

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+}
+
+/*let contador = 0;
 
 while (contador < listaDeTeclas.length) {
   const tecla = listaDeTeclas[contador];
@@ -17,4 +27,4 @@ while (contador < listaDeTeclas.length) {
   };
 
   contador = contador + 1;
-}
+}*/
