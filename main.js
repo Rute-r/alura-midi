f05fa9fcf0494b12064b31bcc66bc77d18368831
Rute-r <rute_ke@hotmail.com>
2023-06-12 12:08:49 -0300

    Adicionando acessibilidade do teclado

--- main.js
+++ main.js
@@ -12,19 +12,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-}
 
-/*let contador = 0;
-
-while (contador < listaDeTeclas.length) {
-  const tecla = listaDeTeclas[contador];
-  const instrumento = tecla.classList[1];
-
-  const idAudio = `#som_${instrumento}`;
-
-  tecla.onclick = function () {
-    tocaSom(idAudio);
+  tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
   };
 
-  contador = contador + 1;
-}*/
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  };
+}
